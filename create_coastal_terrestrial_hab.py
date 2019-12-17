a306a54cd680beda8997fab14eefb6097e47bb0f
Rich Sharp <richpsharp@gmail.com>
2019-12-17 14:26:50 -0800

    making final polygon output

--- create_coastal_terrestrial_hab.py
+++ create_coastal_terrestrial_hab.py
@@ -1,7 +1,8 @@
 """Buffer CV habitat."""
-import sys
+import collections
 import logging
 import os
+import sys
 
 import ecoshard
 from osgeo import gdal
@@ -80,6 +81,23 @@ def main():
         except OSError:
             pass
 
+    # make a consistent set of habitat types
+    risk_distance_to_lulc_code = collections.defaultdict(list)
+    for lulc_code, risk_distance in LULC_CODE_TO_HAB_MAP.items():
+        risk_distance_to_lulc_code[risk_distance].append(lulc_code)
+
+    # this maps all the same type of codes together
+    current_code = -1
+    lulc_code_to_reclass_value = {}
+    hab_code_to_dist_risk = {}
+    for risk_distance_tuple, lulc_code_list in sorted(
+            risk_distance_to_lulc_code.items()):
+        hab_code_to_dist_risk[current_code] = risk_distance_tuple
+        for lulc_code in lulc_code_list:
+            lulc_code_to_reclass_value[lulc_code] = current_code
+        current_code += 1
+    LOGGER.debug(lulc_code_to_reclass_value)
+
     task_graph = taskgraph.TaskGraph(WORKSPACE_DIR, -1, 5)
 
     buffer_vector_path = os.path.join(
@@ -98,13 +116,24 @@ def main():
         target_path_list=[lulc_raster_path],
         task_name='download lulc raster')
 
-    shore_hab_raster_path = os.path.join(CHURN_DIR, 'shore_hab.tif')
+    shore_lulc_raster_path = os.path.join(CHURN_DIR, 'shore_lulc.tif')
     mask_shore_task = task_graph.add_task(
-        func=make_mask,
-        args=(lulc_raster_path, buffer_vector_path, shore_hab_raster_path),
-        target_path_list=[shore_hab_raster_path],
+        func=pygeoprocessing.mask_raster,
+        args=((lulc_raster_path, 1), buffer_vector_path,
+              shore_lulc_raster_path),
+        target_path_list=[shore_lulc_raster_path],
         dependent_task_list=[download_buffer_task, download_lulc_task],
         task_name='mask shore')
+
+    shore_hab_raster_path = os.path.join(CHURN_DIR, 'shore_hab.tif')
+    reclassify_shore_task = task_graph.add_task(
+        func=pygeoprocessing.reclassify_raster,
+        args=(
+            (shore_lulc_raster_path, 1), lulc_code_to_reclass_value,
+            shore_hab_raster_path, gdal.GDT_Int16, -1),
+        target_path_list=[shore_hab_raster_path],
+        dependent_task_list=[mask_shore_task],
+        task_name='shore habitat reclassify')
 
     shore_hab_vector_path = os.path.join(WORKSPACE_DIR, 'shore_hab.gpkg')
     polygonalize_task = task_graph.add_task(
@@ -114,8 +143,53 @@ def main():
         dependent_task_list=[mask_shore_task],
         task_name='polygonalize shore hab')
 
+    shore_hab_risk_vector_path = os.path.join(
+        WORKSPACE_DIR, 'shore_hab_risk_dist.gpkg')
+
+    add_risk_and_prot_dist_task = task_graph.add_task(
+        func=add_risk_dist_to_vector,
+        args=(shore_hab_vector_path, hab_code_to_dist_risk,
+              shore_hab_risk_vector_path),
+        target_path_list=[shore_hab_risk_vector_path])
+
     task_graph.join()
     task_graph.close()
+
+
+def add_risk_dist_to_vector(
+        base_vector_path, base_id_to_dist_risk_map, target_vector_path):
+    """Make a new vector with `risk` and `dist` fields.
+
+    Parameters:
+        base_vector_path (str): path to a vector that contains a field
+            'pix_val'. This value is indexable into `base_id_to_disk_risk_map`.
+        base_id_to_dist_risk_map (dict): maps values in `base_vector`:'pix_val'
+            field to a (risk, distance) tuple to add to the target vector.
+        target_vector_path (str): path to target vector that is a copy of
+            the base vector with `risk` and `dist` fields added to it created
+            by mapping `pix_val` through `base_id_to_disk_risk_map`.
+
+    Returns:
+        None.
+
+    """
+    gpkg_driver = ogr.GetDriverByName('GPKG')
+    base_vector = ogr.Open(base_vector_path)
+    target_vector = gpkg_driver.CopyDataSource(base_vector, target_vector_path)
+    target_layer = target_vector.GetLayer()
+    target_layer.CreateField(ogr.FieldDefn('risk', ogr.OFTReal))
+    target_layer.CreateField(ogr.FieldDefn('dist', ogr.OFTReal))
+    target_layer.StartTransaction()
+    for feature in target_layer:
+        pix_val = feature.GetField('pix_val')
+        risk, dist = base_id_to_dist_risk_map[pix_val]
+        feature.SetField('risk', float(risk))
+        feature.SetField('dist', float(dist))
+        target_layer.SetFeature(feature)
+    target_layer.CommitTransaction()
+    target_layer.SyncToDisk()
+    target_layer = None
+    target_vector = None
 
 
 def polygonalize_raster(raster_path, target_vector_path):
