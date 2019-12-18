95252b7dc1262db93f78df479a7cf147f9bc17b5
Rich Sharp <richpsharp@gmail.com>
2019-12-18 10:42:25 -0800

    adding buffering of lulc and masking per unique risk/dist type

--- global_cv_analysis.py
+++ global_cv_analysis.py
@@ -5,52 +5,84 @@ Design doc is here:
 https://docs.google.com/document/d/18AcJM-rXeIYgEsmqlaUwdtm7gdWLiaD6kkRkpARILlw/edit#heading=h.bbujb61ete53
 
 """
+import collections
 import logging
 import os
 import sys
 
+import ecoshard
 from osgeo import gdal
+import pygeoprocessing
+import taskgraph
 
 WORKING_DIR = "cnc_cv_workspace"
 ECOSHARD_DIR = os.path.join(WORKING_DIR, 'ecoshard')
-_TARGET_NODATA = -1
+TARGET_NODATA = -1
 
-_ECOSHARD_BUCKET_URL = (
+ECOSHARD_BUCKET_URL = (
     r'https://storage.googleapis.com/critical-natural-capital-ecoshards/')
 
-_GLOBAL_GEOMORPHOLOGY_URL = (
-    _ECOSHARD_BUCKET_URL + 'SedType_md5_d670148183cc7f817e26570e77ca3449.zip')
-_GLOBAL_REEFS_URL = (
-    _ECOSHARD_BUCKET_URL + 'Reefs_md5_50b159fff92762ffa5efa0c611b436ce.zip')
-_GLOBAL_MANGROVES_URL = (
-    _ECOSHARD_BUCKET_URL +
+GLOBAL_GEOMORPHOLOGY_URL = (
+    ECOSHARD_BUCKET_URL + 'SedType_md5_d670148183cc7f817e26570e77ca3449.zip')
+GLOBAL_REEFS_URL = (
+    ECOSHARD_BUCKET_URL + 'Reefs_md5_50b159fff92762ffa5efa0c611b436ce.zip')
+GLOBAL_MANGROVES_URL = (
+    ECOSHARD_BUCKET_URL +
     'GMW_001_GlobalMangroveWatch_2016-20191216T181028Z-001_'
     'md5_dd4b04fb9cac2314af99beed2b2c856a.zip')
-_GLOBAL_SEAGRASS_URL = (
-    _ECOSHARD_BUCKET_URL +
+GLOBAL_SEAGRASS_URL = (
+    ECOSHARD_BUCKET_URL +
     'ipbes-cv_seagrass_valid_md5_e206dde7cc9b95ba9846efa12b63d333.gpkg')
+LULC_RASTER_URL = (
+    ECOSHARD_BUCKET_URL +
+    'ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7_'
+    'md5_1254d25f937e6d9bdee5779d377c5aa4.tif')
+BUFFER_VECTOR_URL = (
+    ECOSHARD_BUCKET_URL +
+    'buffered_global_shore_5km_md5_a68e1049c1c03673add014cd29b7b368.gpkg')
 
 
-# tuple form is (path, divide by area?, area to search, extra pixels to add)
-_AGGREGATION_LAYER_MAP = {
-
-    'pdn_gpw': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/gpw-v4-population-count-2015/gpw-v4-population-count_2015.tif"), True, None, 1e3, 0),
-    'pdn_ssp1': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/Spatial_population_scenarios_GeoTIFF/SSP1_GeoTIFF/total/GeoTIFF/ssp1_2050.tif"), True, None, 1e3, 0),
-    'pdn_ssp3': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/Spatial_population_scenarios_GeoTIFF/SSP3_GeoTIFF/total/GeoTIFF/ssp3_2050.tif"), True, None, 1e3, 0),
-    'pdn_ssp5': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/Spatial_population_scenarios_GeoTIFF/SSP5_GeoTIFF/total/GeoTIFF/ssp5_2050.tif"), True, None, 1e3, 0),
-    'pdn_2010': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/Spatial_population_scenarios_GeoTIFF/SSP1_GeoTIFF/total/GeoTIFF/ssp1_2010.tif"), True, None, 1e3, 0),
-    '14bt_pop': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/gpw_v4_e_a000_014bt_2010_cntm_30_sec.tif"), True, None, 1e3, 0),
-    '65plus_pop': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/gpw_v4_e_a065plusbt_2010_cntm_30_sec.tif"), True, None, 1e3, 0),
-    'urbp_2015': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/GLOBIO4_landuse_10sec_tifs_20171207_Idiv/Current2015/Globio4_landuse_10sec_2015_cropint.tif"), False, [1, 190], 5e3, 0),
-    'urbp_ssp1': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/GLOBIO4_landuse_10sec_tifs_20171207_Idiv/SSP1_RCP26/Globio4_landuse_10sec_2050_cropint.tif"), False, [1, 190], 5e3, 0),
-    'urbp_ssp3': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/GLOBIO4_landuse_10sec_tifs_20171207_Idiv/SSP3_RCP70/Globio4_landuse_10sec_2050_cropint.tif"), False, [1, 190], 5e3, 0),
-    'urbp_ssp5': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/GLOBIO4_landuse_10sec_tifs_20171207_Idiv/SSP5_RCP85/Globio4_landuse_10sec_2050_cropint.tif"), False, [1, 190], 5e3, 0),
-    'SLRrate_cur': (("gs://ipbes-natcap-ecoshard-data-for-publication/MSL_Map_MERGED_Global_AVISO_NoGIA_Adjust_md5_3072845759841d0b2523d00fe9518fee.tif"), False, None, 5e3, 1),
-    'slr_rcp26': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/slr_rcp26_md5_7c73cf8a1bf8851878deaeee0152dcb6.tif"), False, None, 5e3, 2),
-    'slr_rcp60': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/slr_rcp60_md5_99ccaf1319d665b107a9227f2bbbd8b6.tif"), False, None, 5e3, 2),
-    'slr_rcp85': ((r"gs://ipbes-natcap-ecoshard-data-for-publication/slr_rcp85_md5_3db20b7e891a71e23602826179a57e4a.tif"), False, None, 5e3, 2),
-}
-
+# This dictionary maps landcode id to (risk, dist) tuples
+LULC_CODE_TO_HAB_MAP = {
+    0: (0, None),
+    10: (0, None),
+    11: (0, None),
+    12: (0, None),
+    20: (0, None),
+    30: (0, None),
+    40: (0, None),
+    50: (1, 2000),
+    60: (1, 2000),
+    61: (1, 2000),
+    62: (1, 2000),
+    70: (1, 2000),
+    71: (1, 2000),
+    72: (1, 2000),
+    80: (1, 2000),
+    81: (1, 2000),
+    82: (1, 2000),
+    90: (1, 2000),
+    100: (1, 2000),
+    110: (2, 2000),
+    120: (2, 2000),
+    121: (2, 2000),
+    122: (2, 2000),
+    130: (2, 2000),
+    140: (2, 2000),
+    150: (4, 500),
+    151: (4, 500),
+    152: (4, 500),
+    153: (4, 500),
+    160: (2, 1000),
+    170: (2, 1000),
+    180: (2, 1000),
+    190: (0, None),
+    200: (0, None),
+    201: (0, None),
+    202: (0, None),
+    210: (0, None),
+    220: (0, None),
+    }
 
 # set a 1GB limit for the cache
 gdal.SetCacheMax(2**30)
@@ -72,3 +104,65 @@ if __name__ == '__main__':
             os.makedirs(dir_path)
         except OSError:
             pass
+
+    task_graph = taskgraph.TaskGraph(CHURN_DIR, -1)
+
+    lulc_raster_path = os.path.join(
+        ECOSHARD_DIR, os.path.basename(LULC_RASTER_URL))
+
+    download_lulc_task = task_graph.add_task(
+        func=ecoshard.download_url,
+        args=(LULC_RASTER_URL, lulc_raster_path),
+        target_path_list=[lulc_raster_path],
+        task_name='download lulc raster')
+
+    shore_buffer_vector_path = os.path.join(
+        ECOSHARD_DIR, os.path.basename(BUFFER_VECTOR_URL))
+    download_buffer_task = task_graph.add_task(
+        func=ecoshard.download_url,
+        args=(BUFFER_VECTOR_URL, shore_buffer_vector_path),
+        target_path_list=[shore_buffer_vector_path],
+        task_name='download global_vector')
+
+    lulc_shore_mask_raster_path = os.path.join(
+        CHURN_DIR, 'lulc_masked_by_shore.tif')
+    mask_lulc_by_shore_task = task_graph.add_task(
+        func=pygeoprocessing.mask_raster,
+        args=((lulc_raster_path, 1), shore_buffer_vector_path,
+              lulc_shore_mask_raster_path),
+        target_path_list=[lulc_shore_mask_raster_path],
+        dependent_task_list=[download_buffer_task, download_lulc_task],
+        task_name='mask shore')
+
+    # each value in `risk_distance_to_lulc_code` can be lumped into one
+    # type.
+    risk_distance_to_lulc_code = collections.defaultdict(list)
+    for lulc_code, risk_distance in LULC_CODE_TO_HAB_MAP.items():
+        risk_distance_to_lulc_code[risk_distance].append(lulc_code)
+
+    # this maps all the same type of codes together
+    lulc_code_to_reclass_value = {}
+    risk_distance_path_map = {}
+    for risk_distance_tuple, lulc_code_list in sorted(
+            risk_distance_to_lulc_code.items()):
+        # reclassify landcover map to be ones everywhere for `lulc_code_list`
+        reclass_map = {}
+        for lulc_code in LULC_CODE_TO_HAB_MAP:
+            if lulc_code in lulc_code_list:
+                reclass_map[lulc_code] = 1
+            else:
+                reclass_map[lulc_code] = 0
+
+        risk_distance_mask_path = os.path.join(
+            CHURN_DIR, '%s_mask.tif' % risk_distance_tuple)
+
+        risk_distance_mask_task = task_graph.add_task(
+            func=pygeoprocessing.reclassify_raster(
+                (lulc_shore_mask_raster_path, 1), reclass_map,
+                risk_distance_mask_path, gdal.GDT_Byte, 0),
+            target_path_list=[risk_distance_mask_path],
+            dependent_task_list=[mask_lulc_by_shore_task],
+            task_name='map distance types %s' % risk_distance_tuple)
+
+    task_graph.join()
+    task_graph.close()
